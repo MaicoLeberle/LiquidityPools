0a4e100a54b131f83bb52fcfaccca97112d3a864
MaicoLeberle <maico.leberle@gmail.com>
2023-03-31 19:14:43 -0300

    Starting major refactor and removing deprecated / mock business logic functions.

--- src/Server/Business.hs
+++ src/Server/Business.hs
@@ -9,15 +9,15 @@ module Business
     , somePool
     , somePools
         -- Actions.
-    , listPools
-    , addFunds
-    , addLiq
-    , createPool
+    -- , listPools
+    -- , addFunds
+    -- , addLiq
+    -- , createPool
     , createUserID
-    , newUserID
-    , rmFunds
-    , rmLiq
-    , swap
+    -- , newUserID
+    -- , rmFunds
+    -- , rmLiq
+    -- , swap
     ) where
 
 import           Data.List
@@ -66,209 +66,209 @@ newUserID n = map (vals !!) <$> (randomChar n 0 $ length vals - 1)
     randomChar :: Int -> Int -> Int -> IO [Int]
     randomChar n a b = sequence $ take n $ repeat $ randomRIO (a, b)
 
-listPools :: [Pool]
-listPools = somePools
+-- listPools :: [Pool]
+-- listPools = somePools
 
--- POST requests.
-{-  The number of new LP tokens is given by the underlying theory--c.f.
-    "Formal Specification of Constant Product (x * y = k) Market Maker Model and
-    Implementation": <https://github.com/runtimeverification/verified-smart-contracts/blob/c40c98d6ae35148b76742aaaa29e6eaa405b2f93/uniswap/x-y-k.pdf>
--}
-createPool :: CreatePoolParams -> Maybe CreatePoolRes
-createPool CreatePoolParams{..} =
-    Just $ mkCreatePoolRes
-         $ floor
-         $ sqrt
-         $ fromInteger
-         $ aAmount (lAssetA cppLiq) * aAmount (lAssetB cppLiq)
+-- -- POST requests.
+-- {-  The number of new LP tokens is given by the underlying theory--c.f.
+--     "Formal Specification of Constant Product (x * y = k) Market Maker Model and
+--     Implementation": <https://github.com/runtimeverification/verified-smart-contracts/blob/c40c98d6ae35148b76742aaaa29e6eaa405b2f93/uniswap/x-y-k.pdf>
+-- -}
+-- createPool :: CreatePoolParams -> Maybe CreatePoolRes
+-- createPool CreatePoolParams{..} =
+--     Just $ mkCreatePoolRes
+--          $ floor
+--          $ sqrt
+--          $ fromInteger
+--          $ aAmount (lAssetA cppLiq) * aAmount (lAssetB cppLiq)
 
-addFunds :: Account -> AddFundsParams -> Maybe AddFundsRes
-addFunds a@Account{..} AddFundsParams{..} = addFundsAux afpFunds []
-  where
-    addFundsAux :: [Asset] -> [Asset] -> Maybe AddFundsRes
-    addFundsAux [] ff' = Just $ mkAddFundsRes $ a { aAssets = ff'}
-    addFundsAux (f : ff) ff' | aAmount f <= 0 = Nothing
-                             |      otherwise =
-        case find ((==) (aName f) . aName) aAssets of
-            Just f' -> addFundsAux ff $ mkAsset (aName f)
-                                                (aAmount f' + aAmount f) : ff'
-            Nothing -> addFundsAux ff $ mkAsset (aName f) (aAmount f) : ff'
+-- addFunds :: Account -> AddFundsParams -> Maybe AddFundsRes
+-- addFunds a@Account{..} AddFundsParams{..} = addFundsAux afpFunds []
+--   where
+--     addFundsAux :: [Asset] -> [Asset] -> Maybe AddFundsRes
+--     addFundsAux [] ff' = Just $ mkAddFundsRes $ a { aAssets = ff'}
+--     addFundsAux (f : ff) ff' | aAmount f <= 0 = Nothing
+--                              |      otherwise =
+--         case find ((==) (aName f) . aName) aAssets of
+--             Just f' -> addFundsAux ff $ mkAsset (aName f)
+--                                                 (aAmount f' + aAmount f) : ff'
+--             Nothing -> addFundsAux ff $ mkAsset (aName f) (aAmount f) : ff'
 
-rmFunds :: Account -> RmFundsParams -> Maybe RmFundsRes
-rmFunds a@Account{..} RmFundsParams{..} = rmFundsAux rfpFunds []
-  where
-    rmFundsAux :: [Asset] -> [Asset] -> Maybe RmFundsRes
-    rmFundsAux [] res = Just $ RmFundsRes $ a {aAssets = res}
-    rmFundsAux (f : ff) ff' | aAmount f <= 0 = Nothing
-                            |      otherwise =
-        case find ((==) (aName f) . aName) aAssets of
-            Just f' | aAmount f' < aAmount f -> Nothing
-                    |              otherwise ->
-                        rmFundsAux ff $ mkAsset (aName f)
-                                                (aAmount f' - aAmount f ) : ff'
-            Nothing -> Nothing
+-- rmFunds :: Account -> RmFundsParams -> Maybe RmFundsRes
+-- rmFunds a@Account{..} RmFundsParams{..} = rmFundsAux rfpFunds []
+--   where
+--     rmFundsAux :: [Asset] -> [Asset] -> Maybe RmFundsRes
+--     rmFundsAux [] res = Just $ RmFundsRes $ a {aAssets = res}
+--     rmFundsAux (f : ff) ff' | aAmount f <= 0 = Nothing
+--                             |      otherwise =
+--         case find ((==) (aName f) . aName) aAssets of
+--             Just f' | aAmount f' < aAmount f -> Nothing
+--                     |              otherwise ->
+--                         rmFundsAux ff $ mkAsset (aName f)
+--                                                 (aAmount f' - aAmount f ) : ff'
+--             Nothing -> Nothing
 
-{-  FIX: Implement preservation of the constant factor invariant and compute the
-    right number of LP tokens.
-    In particular, note that we assume here that asset `aAssetA` will be added
-    entirely to the liquidity of the pool, while only an appropriate amount of
-    asset aAssetB will be added in order to preserve the constant factor
-    invariant. In some cases, however, this may not be correct direction of
-    restrictions, as we might need to proceed in the inverse direction: adding
-    the entire asset `aAssetB` and only an appropriate amount of asset `aAssetA`
-    to preserve the invariant. Consider, for example, the equation for
-    `aBAmount`, where the underlying notion of alpha, according to the
-    terminology used in the theoretical foundations for constant factor
-    automated market makers, clearly assumes that the proportion between assets
-    will be preserved simply by restricting the amount of asset `aAssetB` that
-    is effectively added. A similar incorrect approach is taken for the
-    computation of the number of new LP tokens.
--}
-addLiq :: Pool -> AddLiqParams -> Maybe AddLiqRes
-addLiq p@Pool{..} AddLiqParams{..}
-    | unacceptableParams = Nothing
-    | otherwise = do
-        newAccount <-
-            rfrAccount <$> rmFunds alpAccount
-                (mkRmFundsParams [ aAssetA { aAmount = 0 }
-                                 , aAssetB { aAmount =
-                                                    aAmount aAssetB - aBAmount }
-                                 ])
-        mkRes (pAAmount + aAAmount)
-              (pBAmount + aBAmount)
-              (pLiqTokens + newLiqTokens)
-              newAccount
-  where
-    unacceptableParams :: Bool
-    unacceptableParams =    aAAmount < 0
-                         || aBAmount < 0
-                         || pAAmount /= aAAmount
-                         || pBAmount /= aBAmount
+-- {-  FIX: Implement preservation of the constant factor invariant and compute the
+--     right number of LP tokens.
+--     In particular, note that we assume here that asset `aAssetA` will be added
+--     entirely to the liquidity of the pool, while only an appropriate amount of
+--     asset aAssetB will be added in order to preserve the constant factor
+--     invariant. In some cases, however, this may not be correct direction of
+--     restrictions, as we might need to proceed in the inverse direction: adding
+--     the entire asset `aAssetB` and only an appropriate amount of asset `aAssetA`
+--     to preserve the invariant. Consider, for example, the equation for
+--     `aBAmount`, where the underlying notion of alpha, according to the
+--     terminology used in the theoretical foundations for constant factor
+--     automated market makers, clearly assumes that the proportion between assets
+--     will be preserved simply by restricting the amount of asset `aAssetB` that
+--     is effectively added. A similar incorrect approach is taken for the
+--     computation of the number of new LP tokens.
+-- -}
+-- addLiq :: Pool -> AddLiqParams -> Maybe AddLiqRes
+-- addLiq p@Pool{..} AddLiqParams{..}
+--     | unacceptableParams = Nothing
+--     | otherwise = do
+--         newAccount <-
+--             rfrAccount <$> rmFunds alpAccount
+--                 (mkRmFundsParams [ aAssetA { aAmount = 0 }
+--                                  , aAssetB { aAmount =
+--                                                     aAmount aAssetB - aBAmount }
+--                                  ])
+--         mkRes (pAAmount + aAAmount)
+--               (pBAmount + aBAmount)
+--               (pLiqTokens + newLiqTokens)
+--               newAccount
+--   where
+--     unacceptableParams :: Bool
+--     unacceptableParams =    aAAmount < 0
+--                          || aBAmount < 0
+--                          || pAAmount /= aAAmount
+--                          || pBAmount /= aBAmount
 
-    pAName :: Currency
-    pAName = aName pAssetA
+--     pAName :: Currency
+--     pAName = aName pAssetA
 
-    pAAmount :: Integer
-    pAAmount = aAmount pAssetA
+--     pAAmount :: Integer
+--     pAAmount = aAmount pAssetA
 
-    pAssetA :: Asset
-    pAssetA = lAssetA pLiq
+--     pAssetA :: Asset
+--     pAssetA = lAssetA pLiq
 
-    pBName :: Currency
-    pBName = aName pAssetB
+--     pBName :: Currency
+--     pBName = aName pAssetB
 
-    pBAmount :: Integer
-    pBAmount = aAmount pAssetB
+--     pBAmount :: Integer
+--     pBAmount = aAmount pAssetB
 
-    pAssetB :: Asset
-    pAssetB = lAssetB pLiq
+--     pAssetB :: Asset
+--     pAssetB = lAssetB pLiq
 
-    aA = aName aAssetA
-    aAAmount = aAmount aAssetA
-    aAssetA = lAssetA alpLiq
+--     aA = aName aAssetA
+--     aAAmount = aAmount aAssetA
+--     aAssetA = lAssetA alpLiq
 
-    aB = aName aAssetB
-    aBAmount = (aAAmount * pBAmount) `div` pAAmount
-    aAssetB = lAssetB alpLiq
+--     aB = aName aAssetB
+--     aBAmount = (aAAmount * pBAmount) `div` pAAmount
+--     aAssetB = lAssetB alpLiq
 
-    newLiqTokens :: Integer
-    newLiqTokens = (aAAmount * pLiqTokens) `div` pAAmount
+--     newLiqTokens :: Integer
+--     newLiqTokens = (aAAmount * pLiqTokens) `div` pAAmount
 
-    mkRes :: Integer -> Integer -> Integer -> Account -> Maybe AddLiqRes
-    mkRes a b c =
-        Just . mkAddLiqRes p {pLiq = pLiq { lAssetA = pAssetA {aAmount = a}
-                                          , lAssetB = pAssetB {aAmount = b}
-                                          }
-                             , pLiqTokens = c
-                             }
+--     mkRes :: Integer -> Integer -> Integer -> Account -> Maybe AddLiqRes
+--     mkRes a b c =
+--         Just . mkAddLiqRes p {pLiq = pLiq { lAssetA = pAssetA {aAmount = a}
+--                                           , lAssetB = pAssetB {aAmount = b}
+--                                           }
+--                              , pLiqTokens = c
+--                              }
 
-rmLiq :: Pool -> RmLiqParams -> Maybe RmLiqRes
-rmLiq p@Pool{..} RmLiqParams{..}
-    | unacceptableParams = Nothing
-    |          otherwise = updAccount >>= Just . mkRmLiqRes (p {pLiq = newpLiq})
-  where
-    unacceptableParams :: Bool
-    unacceptableParams = rlpLiqTokens < 0 || rlpLiqTokens > pLiqTokens
+-- rmLiq :: Pool -> RmLiqParams -> Maybe RmLiqRes
+-- rmLiq p@Pool{..} RmLiqParams{..}
+--     | unacceptableParams = Nothing
+--     |          otherwise = updAccount >>= Just . mkRmLiqRes (p {pLiq = newpLiq})
+--   where
+--     unacceptableParams :: Bool
+--     unacceptableParams = rlpLiqTokens < 0 || rlpLiqTokens > pLiqTokens
 
-    newpLiq :: Liq
-    newpLiq = pLiq { lAssetA = mkAsset (aName assetA) (availableA - rmAAmount)
-                   , lAssetB = mkAsset (aName assetB) (availableB - rmBAmount)
-                   }
+--     newpLiq :: Liq
+--     newpLiq = pLiq { lAssetA = mkAsset (aName assetA) (availableA - rmAAmount)
+--                    , lAssetB = mkAsset (aName assetB) (availableB - rmBAmount)
+--                    }
 
-    assetA :: Asset
-    assetA = lAssetA pLiq
+--     assetA :: Asset
+--     assetA = lAssetA pLiq
 
-    availableA :: Integer
-    availableA = aAmount assetA
+--     availableA :: Integer
+--     availableA = aAmount assetA
 
-    rmAAmount :: Integer
-    rmAAmount = (rlpLiqTokens * availableA) `div` pLiqTokens
+--     rmAAmount :: Integer
+--     rmAAmount = (rlpLiqTokens * availableA) `div` pLiqTokens
 
-    assetB :: Asset
-    assetB = lAssetB pLiq
+--     assetB :: Asset
+--     assetB = lAssetB pLiq
 
-    availableB :: Integer
-    availableB = aAmount assetB
+--     availableB :: Integer
+--     availableB = aAmount assetB
 
-    rmBAmount :: Integer
-    rmBAmount = (rlpLiqTokens * availableB) `div` pLiqTokens
+--     rmBAmount :: Integer
+--     rmBAmount = (rlpLiqTokens * availableB) `div` pLiqTokens
 
-    updAccount :: Maybe Account
-    updAccount =
-        do afr <- addFunds rlpAccount
-                           $ mkAddFundsParams [ mkAsset (aName assetA) rmAAmount
-                                              , mkAsset (aName assetB) rmBAmount
-                                              ]
-           Just $ afrAccount afr
+--     updAccount :: Maybe Account
+--     updAccount =
+--         do afr <- addFunds rlpAccount
+--                            $ mkAddFundsParams [ mkAsset (aName assetA) rmAAmount
+--                                               , mkAsset (aName assetB) rmBAmount
+--                                               ]
+--            Just $ afrAccount afr
 
-    newAssetA :: Asset
-    newAssetA = mkAsset (aName assetA) rmAAmount
+--     newAssetA :: Asset
+--     newAssetA = mkAsset (aName assetA) rmAAmount
 
-    newAssetB :: Asset
-    newAssetB = mkAsset (aName assetB) rmBAmount
+--     newAssetB :: Asset
+--     newAssetB = mkAsset (aName assetB) rmBAmount
 
-swap :: Pool -> SwapParams -> Maybe SwapRes
-swap p@Pool{..} SwapParams{..}
-    | swapAForB =
-        let payOut = ceiling $
-                        (fromIntegral $ (aAmount assetA) * (aAmount assetB))
-                            / (fromIntegral $ aAmount spAsset)
-            newPool = p {pLiq = updLiq (aAmount spAsset) (-payOut) pLiq}
-        in mkRes spAsset (mkAsset (aName $ lAssetB pLiq) payOut) newPool
-    | swapBForA =
-        let payOut = ceiling $
-                        (fromIntegral $ (aAmount assetA) * (aAmount assetB))
-                            / (fromIntegral $ aAmount spAsset)
-            newPool = p {pLiq = updLiq (-(aAmount spAsset)) payOut pLiq}
-        in mkRes spAsset (mkAsset (aName $ lAssetA pLiq) payOut) newPool
-    | otherwise = Nothing
-  where
-    swapAForB :: Bool
-    swapAForB = aName spAsset == aName (lAssetA pLiq)
+-- swap :: Pool -> SwapParams -> Maybe SwapRes
+-- swap p@Pool{..} SwapParams{..}
+--     | swapAForB =
+--         let payOut = ceiling $
+--                         (fromIntegral $ (aAmount assetA) * (aAmount assetB))
+--                             / (fromIntegral $ aAmount spAsset)
+--             newPool = p {pLiq = updLiq (aAmount spAsset) (-payOut) pLiq}
+--         in mkRes spAsset (mkAsset (aName $ lAssetB pLiq) payOut) newPool
+--     | swapBForA =
+--         let payOut = ceiling $
+--                         (fromIntegral $ (aAmount assetA) * (aAmount assetB))
+--                             / (fromIntegral $ aAmount spAsset)
+--             newPool = p {pLiq = updLiq (-(aAmount spAsset)) payOut pLiq}
+--         in mkRes spAsset (mkAsset (aName $ lAssetA pLiq) payOut) newPool
+--     | otherwise = Nothing
+--   where
+--     swapAForB :: Bool
+--     swapAForB = aName spAsset == aName (lAssetA pLiq)
 
-    assetA :: Asset
-    assetA = lAssetA pLiq
+--     assetA :: Asset
+--     assetA = lAssetA pLiq
 
-    swapBForA :: Bool
-    swapBForA = aName spAsset == aName (lAssetB pLiq)
+--     swapBForA :: Bool
+--     swapBForA = aName spAsset == aName (lAssetB pLiq)
 
-    assetB :: Asset
-    assetB = lAssetB pLiq
+--     assetB :: Asset
+--     assetB = lAssetB pLiq
 
-    updLiq :: Integer -> Integer -> Liq -> Liq
-    updLiq a' b' l@Liq{ lAssetA = aAsset@Asset{ aAmount = a }
-                      , lAssetB = bAsset@Asset{ aAmount = b }
-                      }
-                 =  l { lAssetA = aAsset{ aAmount = a + a' }
-                      , lAssetB = bAsset{ aAmount = b + b' }
-                      }
+--     updLiq :: Integer -> Integer -> Liq -> Liq
+--     updLiq a' b' l@Liq{ lAssetA = aAsset@Asset{ aAmount = a }
+--                       , lAssetB = bAsset@Asset{ aAmount = b }
+--                       }
+--                  =  l { lAssetA = aAsset{ aAmount = a + a' }
+--                       , lAssetB = bAsset{ aAmount = b + b' }
+--                       }
 
-    mkRes :: Asset -> Asset -> Pool -> Maybe SwapRes
-    mkRes removeAsset pay newPool =
-        updAccount spAccount removeAsset pay >>= Just . mkSwapRes newPool pay
+--     mkRes :: Asset -> Asset -> Pool -> Maybe SwapRes
+--     mkRes removeAsset pay newPool =
+--         updAccount spAccount removeAsset pay >>= Just . mkSwapRes newPool pay
 
-    updAccount :: Account -> Asset -> Asset -> Maybe Account
-    updAccount a payment payout =
-        do a' <- rfrAccount <$> (rmFunds a $ mkRmFundsParams [payment])
-           a'' <- afrAccount <$> (addFunds a' $ mkAddFundsParams [payout])
-           Just a''
+--     updAccount :: Account -> Asset -> Asset -> Maybe Account
+--     updAccount a payment payout =
+--         do a' <- rfrAccount <$> (rmFunds a $ mkRmFundsParams [payment])
+--            a'' <- afrAccount <$> (addFunds a' $ mkAddFundsParams [payout])
+--            Just a''
--- src/Server/Server.hs
+++ src/Server/Server.hs
@@ -43,33 +43,33 @@ app :: Application
 app = serve (Proxy :: Proxy API) server
 
 type API =
-         "createPool"   :> ReqBody '[JSON] CreatePoolParams
-                        :> Post '[JSON] (Maybe CreatePoolRes)
-    :<|> "pools"        :> Get '[JSON] [Pool]
+         "pools"        :> Get '[JSON] [Pool]
     :<|> "subscribe"    :> Get '[JSON] SubscribeRes
     :<|> "account"      :> ReqBody '[JSON] GetAccountParams
                         :> Post '[JSON] GetAccountRes
-    :<|> "addLiquidity" :> ReqBody '[JSON] AddLiqParams
-                        :> Post '[JSON] (Maybe AddLiqRes)
-    :<|> "rmLiquidity"  :> ReqBody '[JSON] RmLiqParams
-                        :> Post '[JSON] (Maybe RmLiqRes)
-    :<|> "addFunds"     :> ReqBody '[JSON] AddFundsParams
-                        :> Post '[JSON] (Maybe AddFundsRes)
-    :<|> "rmFunds"      :> ReqBody '[JSON] RmFundsParams
-                        :> Post '[JSON] (Maybe RmFundsRes)
-    :<|> "swap"         :> ReqBody '[JSON] SwapParams
-                        :> Post '[JSON] (Maybe SwapRes)
+    -- :<|> "createPool"   :> ReqBody '[JSON] CreatePoolParams
+    --                     :> Post '[JSON] (Maybe CreatePoolRes)
+    -- :<|> "addLiquidity" :> ReqBody '[JSON] AddLiqParams
+    --                     :> Post '[JSON] (Maybe AddLiqRes)
+    -- :<|> "rmLiquidity"  :> ReqBody '[JSON] RmLiqParams
+    --                     :> Post '[JSON] (Maybe RmLiqRes)
+    -- :<|> "addFunds"     :> ReqBody '[JSON] AddFundsParams
+    --                     :> Post '[JSON] (Maybe AddFundsRes)
+    -- :<|> "rmFunds"      :> ReqBody '[JSON] RmFundsParams
+    --                     :> Post '[JSON] (Maybe RmFundsRes)
+    -- :<|> "swap"         :> ReqBody '[JSON] SwapParams
+    --                     :> Post '[JSON] (Maybe SwapRes)
 
 server :: Server API
-server =      createPool
-         :<|> listPools
+server =      listPools
          :<|> subscribe
          :<|> accountState
-         :<|> addLiquidity
-         :<|> rmLiquidity
-         :<|> addFunds
-         :<|> rmFunds
-         :<|> swap
+         -- :<|> createPool
+         -- :<|> addLiquidity
+         -- :<|> rmLiquidity
+         -- :<|> addFunds
+         -- :<|> rmFunds
+         -- :<|> swap
 
   where
     listPools :: Handler [Pool]
@@ -82,23 +82,23 @@ server =      createPool
     accountState GetAccountParams{..} =
         DB.getAccount gapID `runOnDBAndApply` mkGetAccountRes
 
-    createPool :: CreatePoolParams -> Handler (Maybe CreatePoolRes)
-    createPool = return . B.createPool
+    -- createPool :: CreatePoolParams -> Handler (Maybe CreatePoolRes)
+    -- createPool = return . B.createPool
 
-    addLiquidity :: AddLiqParams -> Handler (Maybe AddLiqRes)
-    addLiquidity = return . B.addLiq B.somePool
+    -- addLiquidity :: AddLiqParams -> Handler (Maybe AddLiqRes)
+    -- addLiquidity = return . B.addLiq B.somePool
 
-    rmLiquidity :: RmLiqParams -> Handler (Maybe RmLiqRes)
-    rmLiquidity = return . B.rmLiq B.somePool
+    -- rmLiquidity :: RmLiqParams -> Handler (Maybe RmLiqRes)
+    -- rmLiquidity = return . B.rmLiq B.somePool
 
-    addFunds :: AddFundsParams -> Handler (Maybe AddFundsRes)
-    addFunds = return . B.addFunds B.someAccount
+    -- addFunds :: AddFundsParams -> Handler (Maybe AddFundsRes)
+    -- addFunds = return . B.addFunds B.someAccount
 
-    rmFunds :: RmFundsParams -> Handler (Maybe RmFundsRes)
-    rmFunds = return . B.rmFunds B.someAccount
+    -- rmFunds :: RmFundsParams -> Handler (Maybe RmFundsRes)
+    -- rmFunds = return . B.rmFunds B.someAccount
 
-    swap :: SwapParams -> Handler (Maybe SwapRes)
-    swap = return . B.swap B.somePool
+    -- swap :: SwapParams -> Handler (Maybe SwapRes)
+    -- swap = return . B.swap B.somePool
 
 
 -- | Auxiliary values.
